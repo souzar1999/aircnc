594299e31a7152d66e4ab8186e9cc372c48898fd
Victor Souza da Rosa <victorsouzar1999@gmail.com>
2019-10-07 20:57:51 -0300

    Frontend desenvolvido e finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -16,7 +18,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-1cjpa.mongodb.net/s
 // req.params = Acessar route params (edição, delete)
 // req.body = Acessar corpo da requisição (criação, edição)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
